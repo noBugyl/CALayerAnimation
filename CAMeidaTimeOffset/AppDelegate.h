5247b93b9bdb21306b67ec9406711d2ce5ed7ed1
袁龙 <yuanlong@kuaigeng.com>
2017-06-13 16:35:37 +0800

    Initial Commit

---
+++ CAMeidaTimeOffset/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAMeidaTimeOffset
+//
+//  Created by yyyy on 2017/6/13.
+//  Copyright © 2017年 yl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAMeidaTimeOffset/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAMeidaTimeOffset
+//
+//  Created by yyyy on 2017/6/13.
+//  Copyright © 2017年 yl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
